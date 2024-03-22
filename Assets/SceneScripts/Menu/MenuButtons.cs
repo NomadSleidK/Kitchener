a45ddac95fe48550f785e2602a48c2c6f4b82668
Artemii <145929482+NomadSleidK@users.noreply.github.com>
2024-03-22 11:46:29 +0300

    Version 6.2

--- Assets/SceneScripts/Menu/MenuButtons.cs
+++ Assets/SceneScripts/Menu/MenuButtons.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+    [SerializeField] private GameObject _levelsMenu;
+
     public void PlayButtonClick()
     {
         SceneManager.LoadScene("Level1Scene");
+    }
+
+    public void LevelsMenuButtonClick()
+    {
+        _levelsMenu.SetActive(true);
+    }
+
+    public void ExitLevelsMenuButtonClick()
+    {
+        _levelsMenu.SetActive(false);
     }
 }
